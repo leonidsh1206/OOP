764b61e982a13be61320ca9a2be3c439aac864cd
leonidsh1206 <leonidsh1206@gmail.com>
2020-07-14 11:29:38 +0300

    lesson14_ver2

--- src/ru/shakov/lesson11/Censor2.java
+++ src/ru/shakov/lesson11/Censor2.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 public class Censor2 {
     public static void main(String[] args) {
 
+        final String s1 = "«цензура»";
+
         //Кулебяка бяка БЯКА Бяка ,ябак кабя бяка, бякаБяка бякА   бяка  Бякабяка
 
         System.out.println("Введите текст:");
@@ -14,8 +16,8 @@ public class Censor2 {
         String string1 = scanner.nextLine();
         System.out.println(string1);
 
-        String string3 = string1.replaceAll("\\b[Б,б][Я,я][К,к][А,а]\\b", "«цензура»");
-        System.out.println(string3);
+        String string2 = string1.replaceAll("\\b[Б,б][Я,я][К,к][А,а]\\b", s1);
+        System.out.println(string2);
 
     }
 }
--- src/ru/shakov/lesson11/Cenzor.java
+++ src/ru/shakov/lesson11/Cenzor.java
@@ -5,26 +5,30 @@ import java.util.Scanner;
 
 public class Cenzor {
     public static void main(String[] args) {
+
         //"Кулебяка бяка БЯКА Бяка бякаБяка бякА Бякабяка";
+
+        final String s1 = "бяка";
+        final String s2 = "«цензура»";
 
         System.out.println("Введите текст:");
         Scanner scanner = new Scanner(System.in);
 
-        String string3 = scanner.nextLine();
-        System.out.println(string3);
+        String string1 = scanner.nextLine();
+        System.out.println(string1);
 
-        String[] string4 = string3.split(" ");
-        System.out.println(Arrays.toString(string4));   // проверка, как разделен введенный
+        String[] string2 = string1.split(" ");
+        System.out.println(Arrays.toString(string2));   // проверка, как разделен введенный
 
-        //int n = string4.length;       //количество слов в веденном тексте
+        //int n = string4.length;       //количество слов в введенном тексте
         //System.out.println(string4.length);
 
         StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < string4.length; i++) {
-            if ("бяка".equalsIgnoreCase(string4[i])) {
-                string4[i] = "«цензура»";
+        for (int i = 0; i < string2.length; i++) {
+            if (s1.equalsIgnoreCase(string2[i])) {
+                string2[i] = s2;
             }
-            stringBuilder.append(" ").append(string4[i]);
+            stringBuilder.append(" ").append(string2[i]);
         }
         System.out.println(stringBuilder);
     }
