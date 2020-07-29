efb55424147c1c0e0f4377d2506c9424c0283edb
leonidsh1206 <leonidsh1206@gmail.com>
2020-07-29 23:40:59 +0300

    lesson21_2_ver1

--- src/ru/shakov/lesson21_2/Array.java
+++ src/ru/shakov/lesson21_2/Array.java
@@ -3,7 +3,7 @@ package ru.shakov.lesson21_2;
 public class Array {
     public static void main(String[] args) {
         // Задаем массив
-        int[] m = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+        int[] m = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
         // Вызываем метод для вывода элементов массива
         showArray(m);
