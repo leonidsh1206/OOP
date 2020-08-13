dfa0d7e6c40a08c5b355318874ad601a8f85a01a
leonidsh1206 <leonidsh1206@gmail.com>
2020-08-13 13:23:36 +0300

    lesson22_ver1

---
+++ src/ru/shakov/lesson22/HardComparator.java
@@ -0,0 +1,57 @@
+package ru.shakov.lesson22;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class HardComparator {
+    public static void main(String[] args) {
+
+
+        Person[] person = new Person[]{
+                new Person("Вова", 21),
+                new Person("Вова", 3),
+                new Person("Вова", 19),
+                new Person("Ваня", 15),
+                new Person("Ваня", 5),
+                new Person("Ваня", 17)
+        };
+        for (Person persons : person) {
+            System.out.println(persons.toString());
+        }
+        System.out.println();
+
+        // Сортировка по возрасту
+        Arrays.sort(person, new PersonAgeComparator());
+        for (Person persons : person) {
+            System.out.println(persons.toString());
+        }
+        System.out.println();
+
+        // Сортировка по имени
+        Arrays.sort(person, new PersonNameComparator());
+        for (Person persons : person) {
+            System.out.println(persons.toString());
+        }
+
+    }
+
+    static class PersonNameComparator implements Comparator<Person> {
+        @Override
+        public int compare(Person o1, Person o2) {
+            return (o1.getName()).compareTo(o2.getName());
+        }
+    }
+
+    static class PersonAgeComparator implements Comparator<Person> {
+        @Override
+        public int compare(Person o1, Person o2) {
+            if (o1.getAge() > o2.getAge()) {
+                return 1;
+            } else if (o1.getAge() < o2.getAge()) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+    }
+}
---
+++ src/ru/shakov/lesson22/Person.java
@@ -0,0 +1,32 @@
+package ru.shakov.lesson22;
+
+public class Person implements Comparable<Person> {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        return 0;
+    }
+}
