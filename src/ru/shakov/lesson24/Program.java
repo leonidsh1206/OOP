5a8437c7f51b198de37cb5e3d45e36700ab2a7d0
leonidsh1206 <leonidsh1206@gmail.com>
2020-08-14 11:22:29 +0300

    lesson24_ver3

--- src/ru/shakov/lesson24/Program.java
+++ src/ru/shakov/lesson24/Program.java
@@ -6,6 +6,7 @@ public class Program {
 
     public static void main(String[] args) {
 
+        // Исходный список
         List<String> words = Arrays.asList("foo", "buzz", "bar", "fork", "bort",
                 "spoon", "!", "dude");
         Set<String> stringSet = new LinkedHashSet<>();
@@ -15,19 +16,24 @@ public class Program {
         }
         System.out.println();
 
-        Set<String> newStringSet = new LinkedHashSet<>();
+        Set<String> newStringSet;
         newStringSet = removeEvenLength(stringSet);
         for (String entry : newStringSet) {
             System.out.println(entry);
         }
     }
+
+    // Метод принимает на вход список. Возвращает список на основе принятого,
+    // но с исключенными "словами" в которых четное количество букв.
     public static Set<String> removeEvenLength(Set<String> set){
         Set<String> newStringSet = new LinkedHashSet<>();
         Iterator<String> iterator = set.iterator();
         String st;
         while (iterator.hasNext()) {
             st = iterator.next();
-            if ((st.length() % 2) == 1) {
+            // Если остаток от деления длины слова не равен 0
+            // (количество символов в слове нечетное), то добавляем это слово в новый список
+            if ((st.length() % 2) != 0) {
                 newStringSet.add(st);
             }
         }
