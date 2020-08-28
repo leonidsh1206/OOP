7bc6294849490b53e0194471a6c0d32c35b7f333
leonidsh1206 <leonidsh1206@gmail.com>
2020-08-28 13:53:38 +0300

    lesson31_ver2

--- src/ru/shakov/lesson31/Program1.java
+++ src/ru/shakov/lesson31/Program1.java
@@ -1,19 +1,23 @@
 package ru.shakov.lesson31;
 
+// Программа проверяет, является ли исходная строка палиндромом, обход с помощью цикла for
 public class Program1 {
     public static void main(String[] args) {
         String string = "121212121";
+        // Преобразование строки в массив символов
         char[] chars = string.toCharArray();
 
-        boolean b = false;
+        boolean b = false;  // Вспомогательная переменная
+        //Обход массива
         for (int i = 0; i < chars.length / 2; i++) {
             if (chars[i] == chars[chars.length - i - 1]) {
                 b = true;
-            } else {
+            } else {    // Если хотя бы раз наблюдается ассиметрия
                 b = false;
             }
         }
 
+        // Вывод сообщения, является ли исходная строка палиндромом
         if (b) {
             System.out.println("Палиндром");
         } else {
--- src/ru/shakov/lesson31/Program2.java
+++ src/ru/shakov/lesson31/Program2.java
@@ -2,24 +2,28 @@ package ru.shakov.lesson31;
 
 import java.util.Stack;
 
+// Программа проверяет, является ли исходная строка палиндромом, используя Stack
 public class Program2 {
     public static void main(String[] args) {
         String string = "Мадам";
+        // Преобразование строки в массив символов
         char[] chars = string.toCharArray();
 
+        // Добавлем элементы массива в Стек
         Stack stack = new Stack();
         for (char ch : chars) {
-            System.out.println(stack.push(ch));
+            System.out.print(stack.push(ch) + " ");
         }
         System.out.println();
 
+        // Создаем новую строку и добавляем в нее извлекаемы из стека элементы
         String st = "";
         while (!stack.isEmpty()) {
             st += stack.pop();
         }
-        System.out.println(st);
+        System.out.println(st); // Выводим получившуюся строку
 
-        if (st.equals(string)) {
+        if (st.equals(string)) {    // сравниваем получившуюся строку с исходной и выводим, являетс яли она палиндромом
             System.out.println("Палиндром");
         } else {
             System.out.println("Не палиндром");
