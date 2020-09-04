fe973827ffa357707030facaf56c14e38169ace8
leonidsh1206 <leonidsh1206@gmail.com>
2020-09-04 13:37:39 +0300

    lesson29_ver3

--- src/ru/shakov/lesson29/Test.java
+++ src/ru/shakov/lesson29/Test.java
@@ -21,7 +21,7 @@ public class Test {
         book.put("Key6", person4);
 
         // Обход коллекции, вывод в консоль
-        for (Map.Entry e : book.entrySet()) {
+        for (Map.Entry<String, Person> e : book.entrySet()) {
             System.out.println(e.toString());
         }
         System.out.println();
@@ -35,7 +35,7 @@ public class Test {
         System.out.println();
 
         // Вывод в консоль, демонстрация изменений
-        for (Map.Entry e : book.entrySet()) {
+        for (Map.Entry<String, Person> e : book.entrySet()) {
             System.out.println(e.toString());
         }
         System.out.println();
@@ -49,7 +49,7 @@ public class Test {
         System.out.println();
 
         // Вывод в консоль, демонстрация изменений
-        for (Map.Entry e : book.entrySet()) {
+        for (Map.Entry<String, Person> e : book.entrySet()) {
             System.out.println(e.toString());
         }
         System.out.println();
@@ -61,12 +61,12 @@ public class Test {
     }
 
     // Функция, которая выводит сообщение, если в Map-е имеются дубликаты
-    public static void removeTheDuplicates(Map<String, Person> map) {
+    private static void removeTheDuplicates(Map<String, Person> map) {
 
-        for (Map.Entry e : map.entrySet()) {    // Обходим исходную Map
+        for (Map.Entry<String, Person> e : map.entrySet()) {    // Обходим исходную Map
             int i = 0;          // Ввоодим счетчик количества, сколько раз встречается позиция
             // Повторно обходим исходную Map в другом цикле, сравниваем все элементы с каждым в отдельности
-            for (Map.Entry ee : map.entrySet()) {
+            for (Map.Entry<String, Person> ee : map.entrySet()) {
                 if (e.getValue().equals(ee.getValue())) {
                     i++;            // В итоге i (счетчик) будет равен количеству, сколько раз значение встречается в Map
                     if (i > 1) {    // Если элемент встречается минимум 2 раза, то выводим сообщение
@@ -80,8 +80,8 @@ public class Test {
     }
 
     // Функция удаляет указанный элемент из исходной Map
-    public static void removeItemFromMapByValue(Map<String, Person> map, Person value) {
-        for (Map.Entry pair : map.entrySet()) {     // Обход Map
+    private static void removeItemFromMapByValue(Map<String, Person> map, Person value) {
+        for (Map.Entry<String, Person> pair : map.entrySet()) {     // Обход Map
             if (pair.getValue().equals(value)) {    // Если встречается элемент, равный указанному элементу
                 map.remove(pair.getKey());
                 System.out.println("Удалена позиция " + pair);
